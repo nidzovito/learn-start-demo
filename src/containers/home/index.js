61dbb7e07efca526184fdd79dadc9f1609336cbf
nidzovito <nidzovito@gmail.com>
2019-11-26 10:57:31 +0900

    configured routes

---
+++ src/containers/home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const HomeContainer = () => (
+  <h1>Home</h1>
+);
+
+export default HomeContainer;
---
+++ src/containers/stories/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const StoriesContainer = () => (
+  <h1>Stories</h1>
+);
+
+export default StoriesContainer;
